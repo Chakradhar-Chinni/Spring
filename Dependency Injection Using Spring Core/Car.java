4cf8d5820fb8b431e34f7d556c94ba8c7598e6c0
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-02 16:48:35 +0530

    Create Car.java

---
+++ Dependency Injection Using Spring Core/Car.java
@@ -0,0 +1,13 @@
+package com.youtube.telusko.SpringDemo;
+
+public class Car implements Vehicle 
+{
+	public void drive()
+	{
+		System.out.println("Driving a Car");
+	}
+	public void run()
+	{
+		System.out.println("Car Royce");
+	}
+}
