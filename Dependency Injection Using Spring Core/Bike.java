b3db7cb87a7e2934b2c1f2918ce92ed5feb7e887
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-02 16:48:48 +0530

    Create Bike.java

---
+++ Dependency Injection Using Spring Core/Bike.java
@@ -0,0 +1,13 @@
+package com.youtube.telusko.SpringDemo;
+
+public class Bike implements Vehicle
+{
+	public void drive()
+	{
+		System.out.println("Driving a Bike");
+	}
+	public void run()
+	{
+		System.out.println("Running From Bike");
+	}
+}
